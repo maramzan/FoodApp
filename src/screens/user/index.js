9c9c8cfd5595b9da61626051dc62751cbb291db9
maramzan <arsalanramzan811@gmail.com>
2022-01-27 17:26:33 +0500

    font , color and icon added

--- src/screens/user/index.js
+++ src/screens/user/index.js
@@ -1,10 +1,16 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import colors from '../../assets/colors/colors';
 
+// Icon.loadFont();
 const User = () => {
   return (
     <View>
-      <Text>Hello this is user screen</Text>
+      <Icon name="arrow-down" size={30} />
+      <Text style={{fontFamily: 'Montserrat-Regular', color: colors.secondary}}>
+        Hello this is user screen
+      </Text>
     </View>
   );
 };
